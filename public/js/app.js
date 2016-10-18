9b6aeb9d3010ee1c28773494d702498f1022b593
student <student@ai251lab0216m11.aicala.edmc.stu>
2016-10-18 11:49:05 -0700

    third push

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,34 @@
 /* 
  * app.js, code for Trumphouse game.
+ *module pattern.
+ *mvc pattern.
+ *"fart in the elevator" pattern.
  */
 
+
+var game =(function () {
+
+var trump = {}; // object
+
+var bricks =[]; // array of objects
+
+var arena ={}; // object, entire game canvas
+
+return {
+	trump: trump,
+	bricks: bricks,
+	arena: arena
+}
+
+} );
+
+
+
+
+
+
+
+
+
+
+console.log('in app.js'); //debudding line
